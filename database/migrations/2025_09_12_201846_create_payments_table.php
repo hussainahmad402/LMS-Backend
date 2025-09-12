930ae88dbdef37dda9795a7b4784d02ebbf84c37
hussainahmad402 <hussainahmad9402@gmail.com>
2025-09-13 01:20:50 +0500

    design database and relations

---
+++ database/migrations/2025_09_12_201846_create_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('payments', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('student_id')->constrained('users')->cascadeOnDelete();
+    $table->foreignId('enrollment_id')->constrained()->cascadeOnDelete();
+    $table->decimal('amount', 10, 2);
+    $table->enum('status', ['pending','completed','failed'])->default('pending');
+    $table->string('transaction_id')->unique();
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
